95edaeeeed0f72c888472d4e8b8232675e9c34b3
Joseph <whompwhompjm@gmail.com>
2025-06-02 00:21:32 -0500

    attack

--- Weapons/sciccors.gd
+++ Weapons/sciccors.gd
@@ -3,6 +3,9 @@ extends Node2D
 var attack_damage := 10.0
 var attack_force := 100.0
 
+func _process(delta):
+	look_at(get_global_mouse_position())
+
 func _on_hitbox_body_entered(area):
 	if area is HitboxComponent:
 		
