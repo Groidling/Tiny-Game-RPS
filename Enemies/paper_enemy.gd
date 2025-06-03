1b5e6e7de87a303a2207001beb9ae597d07475aa
Joseph <whompwhompjm@gmail.com>
2025-06-02 19:28:19 -0500

    pause when jumping

--- Enemies/paper_enemy.gd
+++ Enemies/paper_enemy.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
+var can_move = true
 var speed = 200
 
 func move_to_player():
 	velocity = ($"../Rock".position-self.position).normalized()* speed
+	
 func _physics_process(delta):
 	move_and_slide()
-	move_to_player()
-	if velocity != Vector2(0,0):
-		$AnimationPlayer.play("paper walk animation")
+	if can_move == true:
+		move_to_player()
 	else:
-		$AnimationPlayer.stop()
+		velocity = Vector2(0,0)
+	$AnimationPlayer.play("paper walk animation")
+
+func can_move_set(take:bool):
+	can_move = take
