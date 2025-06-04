349ef8404a3c0645262e62817136dd75789b181e
Joseph <whompwhompjm@gmail.com>
2025-06-04 17:03:41 -0500

    heart

--- Enemies/paper_enemy.gd
+++ Enemies/paper_enemy.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var can_move = true
 var speed = 500
+var overlapping_areas := []
+var attack_damage = 1
 
 func move_to_player():
 	velocity = ($"../Rock".position-self.position).normalized()* speed
@@ -16,3 +18,21 @@ func _physics_process(delta):
 
 func can_move_set(take:bool):
 	can_move = take
+
+func _on_hitbox_body_entered(area):
+	if area is HitboxComponent:
+		var hitbox = area
+		
+		var attack = Attack.new()
+		attack.attack_damage = attack_damage
+		attack.attack_position = global_position
+		
+		hitbox.damage(attack)
+
+func apply_damage():
+	for area in overlapping_areas:
+		if area is HitboxComponent:
+			var attack = Attack.new()
+			attack.attack_damage = attack_damage
+			attack.attack_position = global_position
+			area.damage(attack)
